9bc29b4ef8f0617767864f2399707713deb05275
Rohit <Welcome1>
2022-01-08 11:45:48 +0530

    Application Entry Point & Interface _ API basics

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import tkinter as tk
 import logging
 
+from bitmex import get_contracts
+
 logger = logging.getLogger()
 
 logger.setLevel(logging.INFO)
@@ -17,6 +19,21 @@ file_handler.setLevel(logging.DEBUG)
 logger.addHandler(stream_handler)
 logger.addHandler(file_handler)
 
+
 if __name__ == '__main__':
+    bitmex_contract = get_contracts()
     root = tk.Tk()
+    root.config(bg='gray12')
+    i = 0
+    j = 0
+    calibri_font = ("Calibri",11,"normal")
+    for contract in bitmex_contract:
+        label_widget = tk.Label(root,text=contract,bg='gray12',fg='SteelBlue1',width=13,font=calibri_font)
+        label_widget.grid(row=i,column=j,sticky='ew')
+        if i == 4:
+            j+=1
+            i=0
+        else:
+            i+=1
+
     root.mainloop()
